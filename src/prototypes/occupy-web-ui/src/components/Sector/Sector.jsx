46f6d42ec9b31e4db84d7c085732aa7153e03c8f
Tim <tim.ashton.la@gmail.com>
2026-01-14 12:55:05 -0800

    Work on Sector component

--- src/prototypes/occupy-web-ui/src/components/Sector/Sector.jsx
+++ src/prototypes/occupy-web-ui/src/components/Sector/Sector.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react"; 
+
 export default function Sector(props) {
+
+    const [ status, setStatus ] = useState(0);
+    
+    function handleSectorClick() {
+        alert(`${props.sectorId} - ${status}`);
+    }
+
     return (
-        <div id="{props.sectorId}" className="sector" />
+        <div id="{props.sectorId}" className="sector" onClick={ handleSectorClick } />
     )
 }
